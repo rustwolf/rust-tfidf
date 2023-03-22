66d36632b61911352edc0abc3a47266565b501c2
Azhar <azhar.mwm@gmail.com>
2023-03-22 16:28:48 +0530

    Formatte test

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,32 @@
 use std::collections::HashMap;
 use std::default;
-use std::fs::{File, DirEntry};
+use std::fs::{DirEntry, File};
 use std::io::Result;
 use std::path::Path;
 use std::{fs, process::exit};
 use xml::reader::EventReader;
 
 fn main() {
-    
     let _all_documents = HashMap::<String, HashMap<String, i32>>::new();
 
     let dir_path = "docs.gl/gl4";
     let dir = fs::read_dir(&dir_path).unwrap();
     for file in dir {
-        
-
         let path = file.unwrap().path();
 
         let path = path.display().to_string();
-    
-        let content = read_entire_xml_file(&path).unwrap();
-        
-        // println!("{content}");
-    }
 
+        let content = read_entire_xml_file(&path).unwrap();
+
+        println!("{content}");
+    }
 }
 
 fn index_document(document_content: &str) -> HashMap<String, i32> {
     todo!("Not implemented yet");
 }
 
-
-
-fn read_entire_xml_file(path : &str) -> Result<String> {
-
+fn read_entire_xml_file(path: &str) -> Result<String> {
     let file = File::open(path).unwrap_or_else(|err| {
         println!("{}", err);
         exit(1)
@@ -48,15 +41,13 @@ fn read_entire_xml_file(path : &str) -> Result<String> {
             exit(1)
         });
 
-
         match event {
             xml::reader::XmlEvent::Characters(text) => {
                 content.push_str(&text);
                 println!("{text}");
-            },
+            }
             _ => {}
         }
-    
     }
 
     Ok(content)
